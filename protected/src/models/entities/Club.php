23667fd06035776871adceaa6d9efc4d86b47265
Ruslan Abramov <ambirus1@gmail.com>
2018-10-16 16:20:02 +0300

    Update Club.php

--- protected/src/models/entities/Club.php
+++ protected/src/models/entities/Club.php
@@ -10,7 +10,7 @@ class Club
 	private $_visitors;
 	private $_tmpVisitors = [];
 
-    public function setPlaylist(Playlist $playlist)
+	public function setPlaylist(Playlist $playlist)
 	{
 		$this->_playlist = $playlist;
 	}
@@ -21,43 +21,43 @@ class Club
 	}
 
 	public function getVisitors() : Group
-    {
-        return $this->_visitors;
-    }
+	{
+		return $this->_visitors;
+	}
 
-    public function getTmpVisitors()
-    {
-        return $this->_tmpVisitors;
-    }
+	public function getTmpVisitors()
+	{
+		return $this->_tmpVisitors;
+	}
 
-    public function setTmpVisitors($newTmpVisitors)
-    {
-        $this->_tmpVisitors = $newTmpVisitors;
-    }
+	public function setTmpVisitors($newTmpVisitors)
+	{
+		$this->_tmpVisitors = $newTmpVisitors;
+	}
 
 	public function playMusic()
 	{
 		if ($this->_playlist === null || sizeof($this->_playlist) == 0)
 		    throw new Exception('Плейлист не создан - нечего проигрывать!');
 
-        $playlist = $this->_playlist->get();
+		$playlist = $this->_playlist->get();
 
 		foreach ($playlist as $music) {
-		    echo 'Проигрывается музыка: ' . $music->getTitle() . ', жанр: ' . $music->getGenre() . "\n";
-            $this->_notifyObservers($music);
-        }
+		    	echo 'Проигрывается музыка: ' . $music->getTitle() . ', жанр: ' . $music->getGenre() . "\n";
+	    		$this->_notifyObservers($music);
+		}
 	}
 
 	private function _notifyObservers(Music $music)
-    {
-        $visitors = $this->_visitors->get();
+	{
+		$visitors = $this->_visitors->get();
 
-        if (sizeof($visitors) > 0) {
-            $this->_tmpVisitors = $visitors;
-            foreach ($visitors as $visitor) {
-                $visitor->dance($this, $music);
-            }
-        }
-    }
+		if (sizeof($visitors) > 0) {
+		    $this->_tmpVisitors = $visitors;
+		    foreach ($visitors as $visitor) {
+			$visitor->dance($this, $music);
+		    }
+		}
+	}
 	
 }
